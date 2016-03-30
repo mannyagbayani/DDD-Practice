9082ab7c92553c9515af09ebd381adae5c6b71ac
mannyagbayani <manny.agbayani@hotmail.com>
2016-03-30 20:13:06 +1300

    first commit. First Bounded Context

---
+++ DDDInPractice/DDDInPractice.Logic/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    /// <summary>
+    /// Use the abstract modifier in a class declaration 
+    /// to indicate that a class is intended only to be a base class of other classes
+    /// </summary>
+    public abstract class Entity
+    {
+        public long Id { get; private set; }
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(other, obj)) return true;
+            if (GetType() != other.GetType()) return false;
+            if (Id == 0 || other.Id == 0) return false;
+
+            return other.Id == Id;
+         }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+
+        public static bool operator ==(Entity a,Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
---
+++ DDDInPractice/DDDInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    /// <summary>
+    /// the sealed modifier prevents other classes from inheriting from it.
+    /// </summary>
+    public sealed class SnackMachine:Entity
+    {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
+
+
+        public void InsertMoney(Money money)
+        {
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+           // MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+            MoneyInTransaction = Money.None;
+        }
+
+        public SnackMachine()
+        {
+            MoneyInside = Money.None;
+            MoneyInTransaction = Money.None;
+        }
+    }
+}
